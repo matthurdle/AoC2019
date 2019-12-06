75518d8c7814ed0bac7984444a42f00b81e243e7
matthurdle <51037395+matthurdle@users.noreply.github.com>
2019-12-05 23:06:23 -0600

    Extending opcode program
    Extending opcode program

--- AoC2019/Day2/IntCode.cs
+++ AoC2019/Day2/IntCode.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace IntCode
 {
-  class IntCode
+    class IntCode
     {
         public int mainIterator;
         public string fullOpCode;
@@ -33,10 +33,10 @@ namespace IntCode
 
         public void padOpCode()
         {
-            fullOpCode = fullOpCode.PadLeft((5 - (fullOpCode.Length-1)), '0');
+            fullOpCode = fullOpCode.PadLeft(5, '0');
         }
 
-        public int run(int[] intArray)
+        public void run(int[] intArray)
         {
             bool keepRunning = true;
 
@@ -51,12 +51,12 @@ namespace IntCode
                     {
                         case "01":
                             //add -- 3 parameters (num1, num2, saveLocation)
-                            Parameter addNum1 = new Parameter(fullOpCode.Substring(2,1) ,intArray[mainIterator + 1]);
+                            Parameter addNum1 = new Parameter(fullOpCode.Substring(2, 1), intArray[mainIterator + 1]);
                             Parameter addNum2 = new Parameter(fullOpCode.Substring(1, 1), intArray[mainIterator + 2]);
                             Parameter addNum3 = new Parameter(fullOpCode.Substring(0, 1), intArray[mainIterator + 3]);
 
                             //PARAM 1
-                            if(addNum1.mode == "0")
+                            if (addNum1.mode == "0")
                             {
                                 //positional
                                 addNum1.index = addNum1.value;
@@ -65,11 +65,11 @@ namespace IntCode
                             else
                             {
                                 //immediate
-                                addNum1.index = mainIterator+1;
+                                addNum1.index = mainIterator + 1;
                             }
 
                             //PARAM 2
-                            if(addNum2.mode == "0")
+                            if (addNum2.mode == "0")
                             {
                                 //positional
                                 addNum2.index = addNum2.value;
@@ -148,58 +148,247 @@ namespace IntCode
                             break;
                         case "03":
                             //input -- 1 parameter (saveLocation)
+                            Parameter inputNum1 = new Parameter(fullOpCode.Substring(2, 1), intArray[mainIterator + 1]);
+
+                            if (inputNum1.mode == "0")
+                            {
+                                int input = int.Parse(Console.ReadLine());
+                                inputNum1.index = inputNum1.value;
+                                intArray[inputNum1.index] = input;
+                            }
+                            else
+                            {
+                                int input = int.Parse(Console.ReadLine());
+                                inputNum1.index = mainIterator + 1;
+                                inputNum1.value = input;
+
+                                intArray[inputNum1.index] = input;
+                            }
+
                             mainIterator += 2;
                             break;
                         case "04":
-                            //output -- 1 parameter (outputLocation
+                            //output -- 1 parameter (outputLocation)
+                            Parameter outputParam = new Parameter(fullOpCode.Substring(2, 1), intArray[mainIterator + 1]);
+
+                            if (outputParam.mode == "0")
+                            {
+                                Console.WriteLine(intArray[outputParam.value]);
+                            }
+                            else
+                            {
+                                Console.WriteLine(intArray[mainIterator + 1]);
+                            }
                             mainIterator += 2;
+                            break;
+                        case "05":
+                            //Jump-If-True - 2 parameters (param to check, param to jump to)
+                            Parameter jit1 = new Parameter(fullOpCode.Substring(2, 1), intArray[mainIterator + 1]);
+                            Parameter jit2 = new Parameter(fullOpCode.Substring(1, 1), intArray[mainIterator + 2]);
+
+                            if(jit1.mode == "0")
+                            {
+                                //positional
+                                jit1.index = jit1.value;
+                                jit1.value = intArray[jit1.index];
+                            }
+                            else
+                            {
+                                //immediate
+                                jit1.index = mainIterator + 1;
+                            }
+
+                            if(jit2.mode == "0")
+                            {
+                                jit2.index = jit2.value;
+                                jit2.value = intArray[jit2.index];
+                            }
+                            else
+                            {
+                                jit2.index = mainIterator + 3;
+                            }
+
+                            if(jit1.value != 0)
+                            {
+                                mainIterator = jit2.value;
+                            }
+                            else
+                            {
+                                mainIterator += 3;
+                            }
+
+                            break;
+                        case "06":
+                            //Jump-If-False - 2 params (param to check, param to jump to)
+                            Parameter jif1 = new Parameter(fullOpCode.Substring(2, 1), intArray[mainIterator + 1]);
+                            Parameter jif2 = new Parameter(fullOpCode.Substring(1, 1), intArray[mainIterator + 2]);
+
+                            if (jif1.mode == "0")
+                            {
+                                //positional
+                                jif1.index = jif1.value;
+                                jif1.value = intArray[jif1.index];
+                            }
+                            else
+                            {
+                                //immediate
+                                jif1.index = mainIterator + 1;
+                            }
+
+                            if (jif2.mode == "0")
+                            {
+                                jif2.index = jif2.value;
+                                jif2.value = intArray[jif2.index];
+                            }
+                            else
+                            {
+                                jif2.index = mainIterator + 2;
+                            }
+
+                            if (jif1.value == 0)
+                            {
+                                mainIterator = jif2.value;
+                            }
+                            else
+                            {
+                                mainIterator += 3;
+                            }
+                            break;
+                        case "07":
+                            //less than - 3 parameters (value 1, value 2, storage location)
+                            // if value one < value two, then store a 1 in storage location
+                            // if not, store a 0 in storage location
+                            Parameter ltNum1 = new Parameter(fullOpCode.Substring(2, 1), intArray[mainIterator + 1]);
+                            Parameter ltNum2 = new Parameter(fullOpCode.Substring(1, 1), intArray[mainIterator + 2]);
+                            Parameter ltNum3 = new Parameter(fullOpCode.Substring(0, 1), intArray[mainIterator + 3]);
+
+                            //PARAM 1
+                            if (ltNum1.mode == "0")
+                            {
+                                //positional
+                                ltNum1.index = ltNum1.value;
+                                ltNum1.value = intArray[ltNum1.index];
+                            }
+                            else
+                            {
+                                //immediate
+                                ltNum1.index = mainIterator + 1;
+                                ltNum1.value = intArray[ltNum1.index];
+                            }
+
+                            //PARAM 2
+                            if (ltNum2.mode == "0")
+                            {
+                                //positional
+                                ltNum2.index = ltNum2.value;
+                                ltNum2.value = intArray[ltNum2.index];
+                            }
+                            else
+                            {
+                                //immediate
+                                ltNum2.index = mainIterator + 2;
+                                ltNum2.value = intArray[ltNum2.index];
+                            }
+
+                            //PARAM 3
+                            if (ltNum3.mode == "0")
+                            {
+                                //positional
+                                ltNum3.index = ltNum3.value;
+                                ltNum3.value = intArray[ltNum3.index];
+
+                            }
+                            else
+                            {
+                                //immediate
+                                ltNum3.index = mainIterator + 3;
+                                ltNum3.value = intArray[ltNum3.index];
+                            }
+
+                            if (ltNum1.value < ltNum2.value)
+                            {
+                                intArray[ltNum3.index] = 1;
+                            }
+                            else
+                            {
+                                intArray[ltNum3.index] = 0;
+                            }
+
+                            mainIterator += 4;
+                            break;
+                        case "08":
+                            //Equals - 3 params
+                            Parameter eqNum1 = new Parameter(fullOpCode.Substring(2, 1), intArray[mainIterator + 1]);
+                            Parameter eqNum2 = new Parameter(fullOpCode.Substring(1, 1), intArray[mainIterator + 2]);
+                            Parameter eqNum3 = new Parameter(fullOpCode.Substring(0, 1), intArray[mainIterator + 3]);
+
+                            //PARAM 1
+                            if (eqNum1.mode == "0")
+                            {
+                                //positional
+                                eqNum1.index = eqNum1.value;
+                                eqNum1.value = intArray[eqNum1.index];
+                            }
+                            else
+                            {
+                                //immediate
+                                eqNum1.index = mainIterator + 1;
+                                eqNum1.value = intArray[eqNum1.index];
+                            }
+
+                            //PARAM 2
+                            if (eqNum2.mode == "0")
+                            {
+                                //positional
+                                eqNum2.index = eqNum2.value;
+                                eqNum2.value = intArray[eqNum2.index];
+                            }
+                            else
+                            {
+                                //immediate
+                                eqNum2.index = mainIterator + 2;
+                                eqNum2.value = intArray[eqNum2.index];
+                            }
+
+                            //PARAM 3
+                            if (eqNum3.mode == "0")
+                            {
+                                //positional
+                                eqNum3.index = eqNum3.value;
+                                eqNum3.value = intArray[eqNum3.index];
+
+                            }
+                            else
+                            {
+                                //immediate
+                                eqNum3.index = mainIterator + 3;
+                                eqNum3.value = intArray[eqNum3.index];
+                            }
+
+                            if (eqNum1.value == eqNum2.value)
+                            {
+                                intArray[eqNum3.index] = 1;
+                            }
+                            else
+                            {
+                                intArray[eqNum3.index] = 0;
+                            }
+
+                            mainIterator += 4;
                             break;
                         default:
                             break;
                     }
-                } 
+                }
                 else
                 {
                     keepRunning = false;
                 }
 
-                int a = 3;
+                //int a = 3;
 
-                
-            //    if (opCode != 99)
-            //    {
-            //        positionOne = intArray[mainIterator + 1];
-            //        valueOne = intArray[positionOne];
-
-            //        positionTwo = intArray[mainIterator + 2];
-            //        valueTwo = intArray[positionTwo];
-
-            //        savePosition = intArray[mainIterator + 3];
-
-            //        if (opCode == 1)
-            //        {
-            //            result = valueOne + valueTwo;
-            //        }
-            //        else if (opCode == 2)
-            //        {
-            //            result = valueOne * valueTwo;
-            //        }
-            //        else
-            //        {
-            //            Console.WriteLine("Bad Opcode!");
-            //        }
-
-            //        intArray[savePosition] = result;
-            //        mainIterator += 4;
-
-            //    }
-            //    else
-            //    {
-            //        keepRunning = false;
-            //    }
+                //return intArray[0];
             }
-
-            return intArray[0];
         }
     }
 }
--- AoC2019/Day2/Program.cs
+++ AoC2019/Day2/Program.cs
@@ -11,16 +11,22 @@ namespace IntCode
     {
         static void Main(string[] args)
         {
-            string fileContent = File.ReadAllText(@"C:\Users\matt.hurdle\source\repos\AoC2019\AoC2019\input.txt");
+            string fileContent = File.ReadAllText(@"D:\Users\matth\github-repos\AoC2019\input.txt");
+            //string fileContent = "3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9";
+            //string fileContent = "3,3,1105,-1,9,1101,0,0,12,4,12,99,1";
+            //string fileContent = "3,9,8,9,10,9,4,9,99,-1,8";
+            //string fileContent = "3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99";
+            //string fileContent = "3,3,1108,-1,8,3,4,3,99";
+
             string[] cmdArray = fileContent.Split(',');
 
-            int result = 0;
+            //int result = 0;
             
             int[] intArray = Array.ConvertAll(cmdArray, int.Parse);
 
             IntCode program = new IntCode();
                   
-            result = program.run(intArray);
+            program.run(intArray);
 
             Console.ReadLine();
         }
